95ec15a164ac25b34dc779e32903cedfe45d296c
ezz hope <robbinnegood@gmail.com>
2023-09-10 19:40:16 +0300

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-alert("Let's integrate!")
+function givenFunction(x) {
+    // Defined function: f(x) = x/16 + 4sin(x) - 3 
+    return x / 16.0 + 4 * Math.sin(x) - 3.0;
+}
+
+function calculateIntegral(func, start, end) { 
+    let step = 1e-5;
+    let numSteps = Math.round((end - start) / step);
+
+    let result = 0.0;
+    for (let i = 1; i <= numSteps; i++) {   
+        let x_k = start + i * step;
+        result += (step * (func(x_k) + func(x_k - step))) / 2.0;
+    }
+
+    return result;
+}
+
+const start = Number(window.prompt("start", ""));
+const end = Number(window.prompt("end", ""));
+
+console.log(calculateIntegral(givenFunction, start, end))
